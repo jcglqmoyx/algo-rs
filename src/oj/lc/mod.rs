bc86a7075e41706349251504cc024e73a51a7566
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-08-21 23:58:28 +0800

    update

--- src/oj/lc/mod.rs
+++ src/oj/lc/mod.rs
@@ -2,3 +2,4 @@ mod p1;
 mod p2;
 mod p664;
 mod p3007;
+mod p3;
---
+++ src/oj/lc/p3.rs
@@ -0,0 +1,23 @@
+struct Solution {}
+ø
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        use std::cmp::max;
+        let mut res = 0;
+        let s = s.as_bytes();
+        let mut cnt = [0; 128];
+        let mut i = 0;
+        let mut j = 0;
+        while j < s.len() {
+            let u = s[j] as usize;
+            cnt[u] += 1;
+            while cnt[u] > 1 {
+                cnt[s[i] as usize] -= 1;
+                i += 1
+            }
+            res = max(res, j - i + 1);
+            j += 1;
+        }
+        res as i32
+    }
+}
