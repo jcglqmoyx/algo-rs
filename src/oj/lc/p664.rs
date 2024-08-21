8943143ecffd6bc3b116192cf527465ee1b01fff
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-08-21 23:31:01 +0800

    update

--- src/oj/lc/p664.rs
+++ src/oj/lc/p664.rs
@@ -5,18 +5,16 @@ impl Solution {
         use std::cmp::min;
         let n = s.len();
         let s = s.as_bytes();
-        let mut f = vec![vec![0; n + 1]; n + 1];
+        let mut f = vec![vec![0x3f3f3f3f; n + 1]; n + 1];
         for i in (0..n).rev() {
             f[i][i] = 1;
             for j in i + 1..n {
                 if s[i] == s[j] {
                     f[i][j] = f[i][j - 1];
                 } else {
-                    let mut mn = i32::MAX;
                     for k in i..j {
-                        mn = min(mn, f[i][k] + f[k + 1][j]);
+                        f[i][j] = min(f[i][j], f[i][k] + f[k + 1][j]);
                     }
-                    f[i][j] = mn;
                 }
             }
         }
