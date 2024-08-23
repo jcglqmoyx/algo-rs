5cf99af27a4a8a2856fc6d401e0102c8c711bb1a
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-08-23 21:38:17 +0800

    update

--- src/oj/lc/p2.rs
+++ src/oj/lc/p2.rs
@@ -15,6 +15,7 @@ impl ListNode {
         }
     }
 }
+
 impl Solution {
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut l1 = l1;
@@ -22,20 +23,30 @@ impl Solution {
         let mut dummy = ListNode::new(0);
         let mut cur = &mut dummy;
         let mut carry = 0;
+
         while l1.is_some() || l2.is_some() || carry > 0 {
             let mut sum = carry;
+
             if let Some(node) = l1 {
                 sum += node.val;
                 l1 = node.next;
             }
+
             if let Some(node) = l2 {
                 sum += node.val;
                 l2 = node.next;
             }
+
             cur.next = Some(Box::new(ListNode::new(sum % 10)));
             carry = sum / 10;
-            cur = cur.next.as_mut()?;
+
+            if let Some(ref mut next_node) = cur.next {
+                cur = next_node;
+            } else {
+                break;
+            }
         }
+
         dummy.next
     }
 }
